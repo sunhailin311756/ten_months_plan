f18f9349461c019beaca94afda3ee74b40c39043
sunhailin311756 <3117565984@qq.com>
2026-09-02 22:06:35 +0800

    添加变量与 printf 练习代码，配置 .gitignore 忽略 exe

--- code/26.9/26.9.1.c
+++ code/26.9/26.9.1.c
@@ -1,6 +1,10 @@
 #include<stdio.h>
 int main()
 {
-    printf("%s","HELLO,WORLD!");
+    printf("%s","Hello,World!");
+    int age = 20 ,weight = 63, height =165;
+    char name[] = "sun hailin";
+    char kg[] ="kg", cm[] = "cm",sui[] = "岁";
+    printf("\n%s %s \n%s %d %s \n%s %d %s \n%s %d %s","姓名", name ,"年龄",age,sui,"身高",height,cm,"体重",weight,kg);
     return 0;
 }
