e07d43dac70ccf6e6f889ea2998fa63ada3057ac
sunhailin311756 <3117565984@qq.com>
2026-09-03 23:41:29 +0800

    完成C语言大作业：电阻LED计价程序，添加大作业题目

--- code/26.9/26.9.1.c
+++ code/26.9/26.9.1.c
@@ -1,10 +1,27 @@
 #include<stdio.h>
+#define TEX 0.06
+float sum(int num,float price)
+{
+    return num*price*(1+TEX);
+}
 int main()
 {
-    printf("%s","Hello,World!");
-    int age = 20 ,weight = 63, height =165;
-    char name[] = "sun hailin";
-    char kg[] ="kg", cm[] = "cm",sui[] = "岁";
-    printf("\n%s %s \n%s %d %s \n%s %d %s \n%s %d %s","姓名", name ,"年龄",age,sui,"身高",height,cm,"体重",weight,kg);
+    char name[20],day[20];
+    printf("请输入姓名：");
+    scanf("%s", name);
+    printf("请输入今天的日期：");
+    scanf("%s", day);
+    printf("输入电阻数量和LED数量：");
+    int num_R,num_LED;
+    float price_R,price_LED;
+    scanf("%d %d", &num_R, &num_LED);
+    printf("输入电阻单价和LED单价：");
+    scanf("%f %f", &price_R, &price_LED);
+    printf("输入运费");
+    int sum_transport;
+    scanf("%d", &sum_transport);
+    printf("电阻总价为：%.2f\n", sum(num_R, price_R));
+    printf("LED总价为：%.2f\n", sum(num_LED, price_LED));
+    printf("总价为：%.2f\n", sum(num_R, price_R) + sum(num_LED, price_LED) + sum_transport);
     return 0;
 }
